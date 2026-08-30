c3e59b36473a2bb4ed1b0bb2f53372a4144161ae
Ahmed <codingwebsa@gmail.com>
2026-08-31 00:35:34 +0600

    cohere: init

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: [".env.local", ".env"] });
+
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  throw new Error("DB url not set on env.");
+}
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: dbUrl,
+  },
+});
---
+++ src/routes/__root.tsx
@@ -0,0 +1,58 @@
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+
+import ClerkProvider from "../integrations/clerk/provider";
+
+import appCss from "../styles.css?url";
+
+import type { QueryClient } from "@tanstack/react-query";
+
+import type { TRPCRouter } from "#/integrations/trpc/router";
+import type { TRPCOptionsProxy } from "@trpc/tanstack-react-query";
+
+interface MyRouterContext {
+  queryClient: QueryClient;
+
+  trpc: TRPCOptionsProxy<TRPCRouter>;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Chat",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
+  shellComponent: RootDocument,
+});
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <ClerkProvider>{children}</ClerkProvider>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
